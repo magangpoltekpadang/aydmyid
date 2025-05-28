69add67123bedd4d0e209be01b81960ad7405883
ayundaauliarahmi <ayundaauliaaaa@gmail.com>
2025-05-28 08:50:20 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('/dashboard', DashboardController::class);
+Route::resource('/', DashboardController::class);
 Route::resource('vehicle-type', VehicleTypeController::class);
 Route::resource('role', RoleController::class);
 Route::resource('membership-package', MembershipPackageController::class);
